f9a298e06c935318820cc18a17c3b4da408b3d2c
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-09-27 00:50:51 -0400

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 7, 'M', 'Black', 'Friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 4, 'F', 'Brown', 'playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 7, 'M', 'Brown', 'Aggressive', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 2, 'F', 'Black', 'Shy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 10, 'M', 'Black', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 9, 'F', 'Brown', 'Mischievous', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, NULL, 'M', 'Black', NULL, 0);
+
